6cbc9a214774f92d8871c94c8bb3362030be80da
Tiago Filipe <tiago.filipe@compasso.com.br>
2020-08-05 18:14:52 -0300

    Init project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1
+          style={{ backgroundColor: "#fff", color: "#fff", height: "100px" }}
+          className="App-title"
+        >
+          Welcome to React
+        </h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+export default App;
