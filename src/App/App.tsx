db7407a74e913ca93362c01585fac8133a3e9f16
Tiago Filipe <tiago.filipe@compasso.com.br>
2020-08-05 23:30:56 -0300

    Init project

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './App.css';
+import LoginPage from '../LoginPage';
+
+const App = () => (
+  <LoginPage />
+);
+
+export default App;
---
+++ src/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Paper } from '@material-ui/core';
+
+const LoginPage = () => (
+  <Container maxWidth="sm" style={{ height: '100%' }}>
+    <Paper elevation={3} style={{ backgroundColor: '#64c1a6', height: '33%' }}>
+      Login
+    </Paper>
+  </Container>
+);
+
+export default LoginPage;
